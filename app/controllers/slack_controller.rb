7351c512e528a9cd810c0fefd8e1736ddcd39b68
RyomaArai <RyomaArai@RyomaArai-no-MacBook-Air.local>
2016-02-15 16:34:04 +0900

    :new:3分毎に、おはようマンアプリからステータスが通知されているかをチェックする処理を追加しました

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -5,12 +5,13 @@ class SlackController < ApplicationController
 
     if status_code == 10
       post_status_message '起動しました'
+      $has_satus_posted = false
+      check_ohayoman_active.delay
     elsif status_code == 11
       post_status_message '停止しました'
     elsif status_code == 20
       post_status_message '正常に動いています'
-    elsif status_code == 30
-      post_status_message 'インターネットに繋がっていない可能性があります'
+      $has_satus_posted = true
     end
 
     head :ok
@@ -20,5 +21,16 @@ class SlackController < ApplicationController
 
   def post_status_message message
     Slack.chat_postMessage text: "[status] #{message}", username: 'おはようマン', channel: '#ohayoman'
+  end
+
+  def check_ohayoman_active
+    Log.v 'debug', 'check_ohayoman_active called'
+    sleep 180
+    if $has_satus_posted
+      $has_satus_posted = false
+      check_ohayoman_active
+    else
+      post_status_message 'インターネットに繋がっていない可能性があります'
+    end
   end
 end
