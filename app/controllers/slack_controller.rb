b5bddcc3a223da31144ad7fa82743cb9c2ba3f7f
RyomaArai <RyomaArai@RyomaArai-no-MacBook-Air.local>
2016-02-16 16:54:36 +0900

    :new:アプリから、ステータスが通知されているかを継続的にチェックする機能の追加

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,19 +1,21 @@
 class SlackController < ApplicationController
   def post_status
-    # TODO status_codeを取り出してくる処理の実装
     status_code = params[:status_code].to_i
 
     if status_code == 10
       post_status_message '起動しました'
-      $has_satus_posted = false
-      check_ohayoman_active.delay
+      $is_app_active = true
+      $has_status_posted = false
+      Thread.new do
+        check_app_active
+      end
     elsif status_code == 11
       post_status_message '停止しました'
+      $is_app_active = false
     elsif status_code == 20
       post_status_message '正常に動いています'
-      $has_satus_posted = true
+      $has_status_posted = true
     end
-
     head :ok
   end
 
@@ -23,14 +25,16 @@ class SlackController < ApplicationController
     Slack.chat_postMessage text: "[status] #{message}", username: 'おはようマン', channel: '#status_log'
   end
 
-  def check_ohayoman_active
-    Log.v 'debug', 'check_ohayoman_active called'
-    sleep 180
-    if $has_satus_posted
-      $has_satus_posted = false
-      check_ohayoman_active
-    else
-      post_status_message 'インターネットに繋がっていない可能性があります'
+  def check_app_active
+    loop do
+      # 3分毎におはようマンからステータスが送られてくるので、判定の間隔に少し余裕を持たせる
+      $is_app_active? (sleep 180) : break
+      if $has_status_posted
+        $has_status_posted = false
+      else
+        post_status_message 'インターネットに繋がっていない可能性があります'
+        break
+      end
     end
   end
 end
