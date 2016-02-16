42cad5fcdc2ff8f14b0b0db144780c1d76da8079
RyomaArai <RyomaArai@RyomaArai-no-MacBook-Air.local>
2016-02-16 16:47:19 +0900

    Ohayoman専用チャンネルに投稿するように変更

--- config/initializers/slack_api.rb
+++ config/initializers/slack_api.rb
@@ -1,5 +1,5 @@
 require 'slack'
 
 Slack.configure do |config|
-  config.token = "xoxp-10404734480-10407162679-20805017169-21a92c117e"
+  config.token = "xoxp-21532393777-21533416806-21533653766-13b6f8cbfa"
 end
